e283f96309adf8c38ba484d162c785388dc9bf26
Alessandro <alessandrotambellini03@gmail.com>
2023-07-07 14:08:03 +0200

    define compilation and usage of the program

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # psychic-disco
+
+## usage
+1. Make the file **_build.sh_** executable by running the following command in the terminal:  
+`chmod +x build.sh`  
+2. Execute **_build.sh_** by running:  
+`./build.sh`
--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-set -xe
+set -xe 
 
-clang program.c interpreter.c main.c -o psychic-disco
+# Check if Clang is avaiable
+if command -v clang >/dev/null 2>&1; then
+    # Compile with Clang
+    clang main.c program.c interpreter.c -o psychic-disco
+# Otherwise check if G++ is avaiable
+elif command -v g++ >/dev/null 2>&1; then
+    # Compile with g++
+    g++ main.c program.c interpreter.c -o psychic-disco
+else 
+    echo "Error: Neither Clang nor g++ is avaiable."
+    exit 1
+fi
