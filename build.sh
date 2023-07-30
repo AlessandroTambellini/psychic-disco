0d97925a1eae19465f8eda112cd4b6fd4494213f
Giovanni Maria Zanchetta <giovmaz2003@gmail.com>
2023-07-30 19:18:15 +0200

    gcc is already aliased to c compiler in linux

--- build.sh
+++ build.sh
@@ -2,15 +2,4 @@
 
 set -xe 
 
-# Check if Clang is avaiable
-if command -v clang >/dev/null 2>&1; then
-    # Compile with Clang
-    clang main.c program.c interpreter.c -o psychic-disco
-# Otherwise check if G++ is avaiable
-elif command -v g++ >/dev/null 2>&1; then
-    # Compile with g++
-    g++ main.c program.c interpreter.c -o psychic-disco
-else 
-    echo "Error: Neither Clang nor g++ is avaiable."
-    exit 1
-fi
+gcc main.c program.c interpreter.c -o psychic-disco
