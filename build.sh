cf5aa6b6b88754f030cd0a97276eeefce6258ab1
lilale <alessandrotambellini03@gmail.com>
2023-08-03 23:17:29 +0200

    check user compiler to create executable

--- build.sh
+++ build.sh
@@ -2,4 +2,15 @@
 
 set -xe 
 
-clang main.c program.c vm.c -o psychic-disco
+# Check if clang is avaiable
+if command -v clang >/dev/null 2>&1; then
+    # Compile with clang
+    clang main.c program.c interpreter.c -o psychic-disco
+# Otherwise check if gcc is avaiable
+elif command -v gcc >/dev/null 2>&1; then
+    # Compile with gcc
+    gcc main.c program.c interpreter.c -o psychic-disco
+else 
+    echo "Error: Neither clang nor gcc is avaiable."
+    exit 1
+fi
